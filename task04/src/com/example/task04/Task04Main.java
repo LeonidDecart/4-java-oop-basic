af0ba6df1b3306e073f53d992ff4ce52aef97460
lenadekart <decartfamily@vk.com>
2025-03-14 19:45:30 +0500

    Симоненко Леонид. Задача 4.

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,14 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
-
+        Line line1 = new Line(new Point(0,0), new Point(5,5));
+        Point[] points = new Point[7];
+        for (int i = 0; i < 6; i++) {
+            points[i] = new Point(i,i);
+        }
+        points[6] = new Point(1,2);
+        for (Point point: points) {
+            System.out.println(line1+" | isCollinearLine("+point+"): "+line1.isCollinearLine(point));
+        }
     }
 }
