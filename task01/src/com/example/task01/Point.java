dad0aa99a5370a5d97e4254829f74813584b17f5
lenadekart <decartfamily@vk.com>
2025-03-14 17:30:43 +0500

    Симоненко Леонид. Задача 1.

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -13,19 +13,19 @@ public class Point {
     }
 
     void flip() {
-        int temp = this.x;
-        this.x = this.y*(-1);
-        this.y = temp*(-1);
+        int temp = x;
+        x = y*(-1);
+        y = temp*(-1);
     }
 
     double distance(Point point) {
-        int a = this.x - point.x;
-        int b = this.y - point.y;
+        int a = x - point.x;
+        int b = y - point.y;
         return Math.sqrt(a*a + b*b);
     }
 
     public String toString() {
-        return "(" + this.x + ", " + this.y + ")";
+        return "(" + x + ", " + y + ")";
     }
 
     void print() {
