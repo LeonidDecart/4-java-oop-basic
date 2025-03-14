5be51066e707ed6dacb63e6bc3460569034351b1
lenadekart <decartfamily@vk.com>
2025-03-14 18:43:34 +0500

    Симоненко Леонид. Задача 3.

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,13 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
+        ComplexNumber c1 = new ComplexNumber(3, 4);
+        ComplexNumber c2 = new ComplexNumber(1, -2);
 
+        ComplexNumber sum = c1.add(c2);
+        ComplexNumber product = c1.multiply(c2);
+
+        System.out.println("Сумма: " + sum);
+        System.out.println("Произведение: " + product);
     }
 }
